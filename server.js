9d12da947102a0dac461149da35514f7e274d23e
raj <raj229500@gmail.com>
2025-11-19 15:29:09 +0530

    Fixed CORS for Netlify

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://teal-klepon-9a05ff.netlify.app",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.post("/submit", async (req, res) => {
